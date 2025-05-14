df0f9db49e110dbcce2452ffb32a218430e47d8b
Joydip Datta <joydip.datta15@gmail.com>
2025-05-15 00:35:10 +0600

    jjjj

--- Jscript/index.js
+++ Jscript/index.js
@@ -1,3 +1,9 @@
 console.log("Hello World!");
 const money = 835;
+const apple = true;
+const banana = 555;
+const orange = "banana";
+const fruits = apple + " " + banana + " " + orange;
+console.log(fruits);
 console.log(money);
+console.log(typeof money);
